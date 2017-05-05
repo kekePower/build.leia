99c6745a49a62296416708aee7c0a89ac4b2cef3
Stig-Ørjan Smelror <smelror@gmail.com>
2017-05-05 22:53:36 +0200

    Version 1.5.7. Several fixes, clean-ups and other goodies.

--- install.sh
+++ install.sh
@@ -1,31 +1,41 @@
-#!/bin/zsh
+#!/bin/env zsh
 
-echo "Do you want to install build.leia to ~/bin/? (Y/n) \c"
+echo "Do you want to install build.leia to ${HOME}/bin/? (Y/n) \c"
 read answer
 
 if [[ ${answer} == "" || ${answer} == "Y" || ${answer} == "y" ]]; then
 	
 	if [[ ! -d ${HOME}/bin ]]; then
-		echo "~/bin doesn't exist. Creating it now."
-		mkdir ~/bin
+		echo "${HOME}/bin doesn't exist. Creating it now."
+		mkdir ${HOME}/bin
 	fi
 
-	echo "Copying files to ~/bin/"
-	cp -f build.leia ~/bin/
-	if [[ $( cat ~/bin/etc/build.leia.conf | head -1 ) != $( cat etc/build.leia.conf.sample | head -1 ) ]]; then
-		echo "Please review ~/bin/etc/build.leia.conf.sample for changes and/or updates"
-		cp -f etc/build.leia.conf.sample ~/bin/etc/build.leia.conf.sample
-	else
-		cp -af etc ~/bin/
+	echo "Copying files to ${HOME}/bin/"
+	cp -f build.leia ${HOME}/bin/
+
+	if [[ ! -d ${HOME}/bin/etc ]]; then
+		cp -af etc ${HOME}/bin/
 	fi
-	cp -af include ~/bin/
-	chmod 750 ~/bin/build.leia
+
+	if [[ $( cat ${HOME}/bin/etc/build.leia.conf | head -1 ) != $( cat etc/build.leia.conf.sample | head -1 ) ]]; then
+		echo "Please review ${HOME}/bin/etc/build.leia.conf.sample for changes and/or updates"
+		cp -f etc/build.leia.conf.sample ${HOME}/bin/etc/build.leia.conf.sample
+	fi
+
+	cp -af include ${HOME}/bin/
+	chmod 750 ${HOME}/bin/build.leia
+
 	echo "build.leia is installed."
-	echo "Please edit ~/bin/etc/build.leia.conf.sample before running the script."
+	echo "Please edit ${HOME}/bin/etc/build.leia.conf.sample before running the script."
+	echo "Rename it to build.leia.conf when you're done editing it."
+	echo "If ${HOME}/bin is not in your PATH, you will have to add it."
 	echo "Please run \"build.leia --help\" to see your options"
 	echo "Good luck!"
+
 else
+
 	echo "I guess this isn't the right time for you."
 	echo "I understand."
 	echo "Please return if/when you're ready."
+
 fi
